0dfb725878d8a1d1e14c80ca5af94aa8ec073fac
mahmudul-Hasan-2 <mahmudul5790@gmail.com>
2026-07-13 15:38:20 +0600

    Added proxy.ts

---
+++ proxy.ts
@@ -0,0 +1,16 @@
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+// This function can be marked `async` if using `await` inside
+export async function proxy(request: NextRequest) {
+  const session = await auth.api.getSession({ headers: await headers() });
+  if (!session) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+}
+
+export const config = {
+  matcher: ["/items/manage", "/items/add"],
+};
